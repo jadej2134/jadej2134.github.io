e06053ac6e8805c3752359f51acd1c5769fec6d9
jadej2134 <jadej62@nycstudents.net>
2026-02-11 18:51:54 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 let ownerName = "Jade M."; // update the part BETWEEN the "quotes"
-let userName = "jadej2134"; // same here
+let userName = "jade2134"; // same here
 
 document.querySelectorAll(".owner-name").forEach((e) => {
     e.innerHTML = ownerName;
